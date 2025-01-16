5373f322faf352309ca1d4e13c699b45b08368f5
skksharif <khasimsharif12@gmail.com>
2025-01-16 19:19:14 +0530

    Theme changed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,16 @@
-import React,{useEffect} from 'react';
-import AOS from 'aos';
+import React, { useEffect } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
-import './ContactForm.css';
+import "./ContactForm.css";
 
 const ContactForm = () => {
   return (
-    <div className="contact-form1" >
+    <div className="contact-form1">
       <h1>Let's Connect</h1>
-      <p>Contact us and we'll have a Business Development Representative contact you within 24 business hours.</p>
+      <p>
+        Contact us and we'll have a Business Development Representative contact
+        you within 24 business hours.
+      </p>
       <form data-aos="zoom-in-up">
         <div className="form-group">
           <div className="input-container">
@@ -38,9 +41,16 @@ const ContactForm = () => {
             <label>Country / Region *</label>
             <select required>
               <option value="">Select...</option>
-              <option value="us">United States</option>
-              <option value="ca">Canada</option>
-              {/* Add more options as needed */}
+              <option value="cn">China</option>
+              <option value="jp">Japan</option>
+              <option value="in">India</option>
+              <option value="kr">South Korea</option>
+              <option value="sg">Singapore</option>
+              <option value="th">Thailand</option>
+              <option value="my">Malaysia</option>
+              <option value="id">Indonesia</option>
+              <option value="ph">Philippines</option>
+              <option value="vn">Vietnam</option>
             </select>
           </div>
         </div>
@@ -53,7 +63,9 @@ const ContactForm = () => {
             {/* Add more options as needed */}
           </select>
         </div>
-        <button type="submit" className="submit-btn">Submit →</button>
+        <button type="submit" className="submit-btn">
+          Submit →
+        </button>
       </form>
     </div>
   );
